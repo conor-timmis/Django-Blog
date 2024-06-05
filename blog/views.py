e12554f92eb957d6149a08bb7b44e2c269d9ebd2
Conor Timmis <mctto@protonmail.com>
2024-06-05 17:55:38 +0000

    Add template context

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,8 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+        "coder": "Conor Timmis"},
     )
 
 def event_detail(request, event_id):
