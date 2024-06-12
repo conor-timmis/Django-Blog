277720fd01061c48c3d1add809f1968c37e58708
Conor Timmis <mctto@protonmail.com>
2024-06-12 20:08:06 +0000

    Method to store user-generated images

--- about/models.py
+++ about/models.py
@@ -1,16 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
 
 class About(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __str__(self):
         return self.title
-
 
 class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
