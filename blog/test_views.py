b3c2e67b33d5ae2927f36e4ed3074e7ba929d176
Conor Timmis <mctto@protonmail.com>
2024-06-13 14:03:32 +0000

    Test POSTs within blog.views

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,21 +1,39 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import About
-from .forms import CollaborateForm
+from .forms import CommentForm
+from .models import Post
 
+# Create your tests here.
+class TestBlogViews(TestCase):
 
-class TestAboutView(TestCase):
 
     def setUp(self):
-        """Creates about me content"""
-        self.about_content = About(
-            title="About Me", content="This is about me.")
-        self.about_content.save()
+        """Create a superuser and a blog post"""
+        self.user = User.objects.create_superuser(
+            username="myUsername", password="myPassword", email="test@test.com")
+        self.post = Post(title="Blog title", author=self.user,
+                         slug="blog-title", excerpt="Blog excerpt",
+                         content="Blog content", status=1)
+        self.post.save()
 
-    def test_render_about_page_with_collaborate_form(self):
-        """Verifies get request for about me containing a collaboration form"""
-        response = self.client.get(reverse('about'))
+    def test_render_post_detail_page_with_comment_form(self):
+        """Verifies a single blog post containing a comment form is returned"""
+        response = self.client.get(reverse('post_detail', args=['blog-title']))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'About Me', response.content)
+        self.assertIn(b"Blog title", response.content)
+        self.assertIn(b"Blog content", response.content)
         self.assertIsInstance(
-            response.context['collaborate_form'], CollaborateForm)
+            response.context['comment_form'], CommentForm)
+
+    def test_successful_collaboration_request_submission(self):
+        """Test for a user requesting a collaboration"""
+    post_data = {
+        'name': 'test name',
+        'email': 'test@email.com',
+        'message': 'test message'
+    }
+    response = self.client.post(reverse('about'), post_data)
+    self.assertEqual(response.status_code, 200)
+    self.assertIn(
+        b'Collaboration request received! I endeavour to respond within 2 working days.', response.content)
